ae6f317b8453c125d04518a311579d20035a6344
gdubovitskiy <gdubovitskiy@ozon.ru>
2024-03-22 21:07:32 +0300

    Done HW2 tasks

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,3 +4,18 @@
 - NotEnoughFuel
 - CargoOverload
 """
+
+class LowFuelError(Exception):
+    pass
+
+class NotEnoughFuel(Exception):
+    pass
+
+class CargoOverload(Exception):
+    pass
+
+class DistanceNegativeError(Exception):
+    pass
+
+class NegativeFuelConsumption(Exception):
+    pass
