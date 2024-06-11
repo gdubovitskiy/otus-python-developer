48f91c152ca48bf5100f6ff61f313f19f44123a0
gdubovitskiy <gdubovitskiy@ozon.ru>
2024-06-11 21:53:59 +0300

    убрал переопределение SQLALCHEMY_PG_CONN_URI

--- homework_04/models/db.py
+++ homework_04/models/db.py
@@ -14,8 +14,6 @@ from sqlalchemy.ext.asyncio import (create_async_engine,
 
 import configs.config as config
 
-os.environ["SQLALCHEMY_PG_CONN_URI"] = config.DB_URL
-
 PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
 async_engine = create_async_engine(
